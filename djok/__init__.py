d5ba3cf67fc1410f564ee1e2cefdd2b42cec47ad
Vlasov Artem [WS] <root@proscript.ru>
2026-01-01 21:24:14 +0400

    Bump version 0.1.0_rc9

--- djok/__init__.py
+++ djok/__init__.py
@@ -1,4 +1,4 @@
 __version__ = "0.1.0"
 __next_version__ = "0.1.0"
-__build__ = 8
-__commit__ = "5b5ae622b1ee2aba19b98883f05b3c2d9c468e92"
+__build__ = 9
+__commit__ = "8da8350427f48c3c26e51a5e46e47f328e6e728c"
